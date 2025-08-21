f0105871ff203d7c8cee91e8483c7fed03edeebf
NasonovSergey <s0-hard@yandex.ru>
2025-08-21 17:34:45 +0300

    Задание 3.3.2

---
+++ FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Sergey";
+            byte MyAge = 29;
+            bool HaveIApet = false;
+            double MyShoeSize = 42.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+        }
+    }
+}
